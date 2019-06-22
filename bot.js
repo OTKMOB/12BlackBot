35a83b06241c5e66d0eb6fda6c112a0e765fae6d
Yunding Wu <yuw638@ucsd.edu>
2019-06-22 14:21:02 +0800

    test3

--- bot.js
+++ bot.js
@@ -1,13 +1,29 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
-})
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "NTkxODMwMDY4MTYyOTg1OTkz.XQ2ymA.JelO_2hRR7oOjPJKlwRaAX9XiYw"
+    console.log('I am ready!');
 
-client.login(bot_secret_token)
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret

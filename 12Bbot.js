2dd69820faedee51d0f19b0eb82fbab8faf2e7fd
Yunding Wu <yuw638@ucsd.edu>
2019-06-22 17:24:37 +0800

    signUp

--- 12Bbot.js
+++ 12Bbot.js
@@ -16,11 +16,28 @@ client.on('ready', () => {
 client.on('message', message => {
 
     if (message.content === '我要打十二黑') {
-
-        playerNum++;    
-        message.channel.send(`现在已经有${playerNum}个人要打十二黑了`);
-
+        
+        if(message.author.signedUp === true) {
+            message.reply("你已经报过名了");
+        }
+        else {
+            message.author.signedUp = true;
+            playerNum++;    
+            message.channel.send(`现在已经有${playerNum}个人要打十二黑了`);
        }
+    }
+
+    if(message.content === '我不打十二黑了') {
+        
+        if(message.author.signedUp === false) {
+            message.reply("你本来就不打");
+        }
+        else {
+            message.author.signedUp = false;
+            playerNum--;    
+            message.channel.send(`现在还有${playerNum}个人要打十二黑`);
+        }
+    }
 
 });
 

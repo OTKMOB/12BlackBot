d46120634692b80eaeaa50f4cca141aea34c318f
Yunding Wu <yuw638@ucsd.edu>
2019-07-05 17:14:59 +0800

    optimization test 1

--- 12Bbot.js
+++ 12Bbot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const musakui = require('musakui');
+const musakui = require('Musakui.js');
 
 const client = new Discord.Client();
 
@@ -123,13 +123,13 @@ client.on('message', message => {
         .then(result => {
                         if(result.hasOwnProperty('media_url')) {
                             let link = result.media_url;
-                            if(link.endsWith('.webm')) {
+                            /*if(link.endsWith('.webm')) {
                                 let idx = link.indexOf('.webm');
                                 link = link.substring(14, idx);
                                 link = 'https://' + link;
                                 perfect.send(link);
-                            }
-                            else if(link.startsWith('https://v.redd.it/')) {
+                            }*/
+                            /*else*/ if(link.startsWith('https://v.redd.it/')) {
                                 link = 'Discord不支持直接播放Reddit视频，请点击链接查看: \n' + link; 
                                 perfect.send(link);
                             }
@@ -142,6 +142,12 @@ client.on('message', message => {
                         }
                     })
         .catch(error => perfect.send('获取帖子信息失败，请再试一次'));
+    }
+
+    if(message.content === "!test") {
+        musakui('aww')
+        .then(result => console.log(result.media_url))
+        .catch(error => console.log(error));
     }
 
 });
---
+++ Musakui.js
@@ -0,0 +1,105 @@
+/* Originally from https://github.com/justluca/Musakui */
+/*
+MIT License
+
+Copyright (c) 2019 Luca Polesel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-useless-escape */
+/* eslint-disable camelcase */
+const got = require('got');
+
+function randomPost(sub) {
+  return got(`https://www.reddit.com/r/${sub}/random/.json`, { json: true, headers: {
+    'user-agent': 'Chrome'
+  }}).then((resp) => {
+    const {
+      children,
+    } = !Array.isArray(resp.body)
+      ? resp.body.data
+      : resp.body[0].data;
+
+    // Check if it contains something
+    if (children.length < 1) {
+      throw new Error('No posts.');
+    }
+
+    let {
+      data,
+    } = children[0];
+
+    // Check if crossposted
+    if (data.crosspost_parent_list) {
+      data = data.crosspost_parent_list[0];
+    }
+
+    // Parse post
+    const {
+      title,
+      selftext,
+      ups,
+      downs,
+      num_comments,
+      url,
+      permalink,
+      author,
+      over_18,
+      is_self,
+    } = data;
+
+    const obj = {
+      title,
+      content: selftext,
+      author,
+      upvotes: ups,
+      downvotes: downs,
+      comments: num_comments,
+      nsfw: over_18,
+      reddit_url: `https://reddit.com${permalink}`,
+    };
+
+    // Check if this post contains an image/video/gif
+    // TODO: Check more cases
+    /*if (!is_self) {
+      // Get proper media url
+      
+      if (url.startsWith('https://v.redd.')) {
+        obj.media_url = data.media.reddit_video.fallback_url;
+
+      } else if (url.startsWith('https://gfycat.')) {
+        const reg = /[^\/]+(?=\/$|$)/;
+        const match = reg.exec(url);
+
+        if (match) {
+          obj.media_url = `https://gfycat.com/${match[0]}`;
+        }
+      } else {*/
+        obj.media_url = url;
+      //}
+    //}
+
+    return obj;
+  });
+}
+
+module.exports = sub => randomPost(sub);
